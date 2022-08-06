e84496b0edae1db9e78c2a389b9e8eac213f36d1
Junjing Yu <2573038834@qq.com>
2022-08-06 17:53:26 +1000

    support card style

--- frontend/src/components/SupportCards/SupportCardsCSS.js
+++ frontend/src/components/SupportCards/SupportCardsCSS.js
@@ -2,12 +2,15 @@ import styled from 'styled-components';
 
 const CardsContainer = styled.div`
   display: flex;
-  align-items: flex-start;
+  align-items: normal;
+  justify-content: left;
   margin: 20px;
   flex-flow: wrap;
 `
 
 const CardContainer = styled.div`
+  width: 40%;
+  min-width: 300px;
   margin: 20px;
 `
 
