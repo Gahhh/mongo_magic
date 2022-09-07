54530f44cd8284139ac41c866b840bfdc3346cd6
Gahhh <70998894+Gahhh@users.noreply.github.com>
2022-09-07 16:43:03 +1000

    Update config.py

--- backend/cloud/config.py
+++ backend/cloud/config.py
@@ -1,4 +1,5 @@
+# These keys have been deleted for security reason. 
 AWS_S3_ACCESS = {
-  'AWS_ACCESS_KEY_ID': 'AKIA4ZGKVOLI67OLTOXD',
-  'AWS_SECRET_ACCESS_KEY':'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'
+  'AWS_ACCESS_KEY_ID': '',
+  'AWS_SECRET_ACCESS_KEY':''
 }
